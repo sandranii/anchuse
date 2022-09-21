a2c0909e893c9697f635d594c8d04b8643249809
FanWeny <z04wj06y7@gmail.com>
2022-09-21 13:36:59 +0800

    fix gsap

--- src/js/aboutUsScrollTrigger.js
+++ src/js/aboutUsScrollTrigger.js
@@ -26,36 +26,36 @@ gsap.registerPlugin(ScrollTrigger);
         z: -200,
         ease: "back"
     });
-    gsap.from(".text_one p",{
-        scrollTrigger: {
-            trigger: ".frame_one",
-            start: "top 70%"
-        },
-        x: 150,
-        stagger: 0.2,
-        duration: 1,
-        delay: 0.8,
-        opacity: 0,
-        ease: "back"
-    });
-    gsap.from(".text_two p",{
-        scrollTrigger: {
-            trigger: ".frame_two",
-            start: "top 70%"
-        },
-        x: 150,
-        stagger: 0.2,
-        duration: 1,
-        delay: 0.8,
-        opacity: 0,
-        ease: "back"
-    });
+    // gsap.from(".text_one p",{
+    //     scrollTrigger: {
+    //         trigger: ".frame_one",
+    //         start: "top 70%"
+    //     },
+    //     x: 150,
+    //     stagger: 0.2,
+    //     duration: 1,
+    //     delay: 0.8,
+    //     opacity: 0,
+    //     ease: "back"
+    // });
+    // gsap.from(".text_two p",{
+    //     scrollTrigger: {
+    //         trigger: ".frame_two",
+    //         start: "top 70%"
+    //     },
+    //     x: 150,
+    //     stagger: 0.2,
+    //     duration: 1,
+    //     delay: 0.8,
+    //     opacity: 0,
+    //     ease: "back"
+    // });
     gsap.from(".circle",{
         scrollTrigger: {
             trigger: ".name",
             start: "top 70%"
         },
-        y:300,
+        y: 100,
         stagger: 0.1,
         duration: 1.2,
         opacity: 0,
